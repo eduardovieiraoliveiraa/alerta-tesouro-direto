958d14878dff25b7bc44e628a69314509e296414
EDUARDO VIEIRA OLIVEIRA <eduardovieiraoliveiraa@gmail.com>
2024-09-12 19:44:31 -0300

    criacao do projeto

---
+++ src/main/java/br/com/eduardooliveiradev/alerta/tesourodireto/AlertaTesouroDiretoApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardooliveiradev.alerta.tesourodireto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaTesouroDiretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaTesouroDiretoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduardooliveiradev/alerta/tesourodireto/AlertaTesouroDiretoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardooliveiradev.alerta.tesourodireto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaTesouroDiretoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
